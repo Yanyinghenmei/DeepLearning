3096eef21ecc890e64130e70a98f0d83cccade63
Yanyinghenmei <1113135372@qq.om>
2020-04-10 00:38:39 +0800

    layer

--- learn02/apple_roange.py
+++ learn02/apple_roange.py
@@ -3,7 +3,9 @@
 
 __author__ = 'Daniel'
 
-from learn02.layer_native import MulLayer
+import sys, os
+sys.path.append(os.pardir)
+from learn02.layer_native import MulLayer, AddLayer
 
 apple = 100
 apple_num = 2
@@ -13,9 +15,23 @@ tax = 1.1
 
 # layer
 mul_apple_layer = MulLayer()
+mul_orange_layer = MulLayer()
+add_layer = AddLayer()
 mul_tax_layer = MulLayer()
 
 # forward
 apple_price = mul_apple_layer.forward(apple, apple_num)
-price = mul_tax_layer.forward(apple_price, tax)
+orange_price = mul_orange_layer.forward(orange, orange_num)
+all_price = add_layer.forward(apple_price, orange_price)
+price = mul_tax_layer.forward(all_price, tax)
+
+# backward
+dprice = 1
+dall_price, dtax = mul_tax_layer.backwark(dprice)
+dapple_price, dorange_price = add_layer.backward(dall_price)
+dapple, dapple_num = mul_apple_layer.backwark(dapple_price)
+dorange, dorange_num = mul_orange_layer.backwark(dorange_price)
+
+print(price)
+print(dapple_num, dapple, dorange_num, dorange, dtax)
 
