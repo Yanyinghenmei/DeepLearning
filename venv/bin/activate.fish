cbd4f330d1fa91d164a399dd01f05a02b7ffdedb
Yanyinghenmei <1113135372@qq.om>
2020-04-12 17:20:41 +0800

    python version

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/daniel/Desktop/aaa/venv"
+set -gx VIRTUAL_ENV "/Users/daniel/Desktop/learn/DeepLearning-master/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
