9b0260007cde32026e24f19783eb5b50c88ce076
Yanyinghenmei <1113135372@qq.com>
2020-04-17 16:20:28 +0800

    tensorflow

---
+++ pip_install.sh
@@ -0,0 +1,4 @@
+# sh pip_install.sh
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pillow
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple matplotlib
+pip --default-timeout=100 install -i https://pypi.tuna.tsinghua.edu.cn/simple tensorflow
